d22932e931bddf7227e4bf27bf1eb2bcc03ab8a5
Muffin135 <lionelq36@gmail.com>
2020-02-03 19:30:44 -0800

    Cleaned house.

--- Templates/Template functions/Templates.cpp
+++
@@ -1,8 +0,0 @@
-#include "Templates.h"
-
-int max(int a, int b) {
-	return a < b ? b : a;
-}
-
-// Suppose I wanted to use this code with double instead of int. What has to change?
-// Does that seem like an efficient idea?
